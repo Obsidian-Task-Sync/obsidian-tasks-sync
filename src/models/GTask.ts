b03e6f24c6e44dcf3edf9a467c1b829694ac6ee2
hoondeveloper <hoonskyn9@gmail.com>
2025-05-18 15:36:59 +0900

    chore: eslint, prettier 설정, husky hook 추가

--- src/models/GTask.ts
+++ src/models/GTask.ts
@@ -1,43 +1,43 @@
 export enum GTaskItemStatus {
-	NeedsAction = "needsAction",
-	Completed = "completed",
+  NeedsAction = 'needsAction',
+  Completed = 'completed',
 }
 
 export interface GTaskItem {
-	id: string;
-	title: string;
-	status: GTaskItemStatus;
-	due?: string;
-	updated: string;
+  id: string
+  title: string
+  status: GTaskItemStatus
+  due?: string
+  updated: string
 }
 
 export interface GTaskDataSource {
-	get(id: string): Promise<GTaskItem>;
-	list(): Promise<GTaskItem[]>;
-	update(id: string, data: Partial<GTaskItem>): Promise<void>;
-	insert(meta: { title: string; due?: string }): Promise<GTaskItem>;
+  get(id: string): Promise<GTaskItem>
+  list(): Promise<GTaskItem[]>
+  update(id: string, data: Partial<GTaskItem>): Promise<void>
+  insert(meta: { title: string; due?: string }): Promise<GTaskItem>
 }
 
 export class GTask {
-	dataSource: GTaskDataSource;
+  dataSource: GTaskDataSource
 
-	constructor(dataSource: GTaskDataSource) {
-		this.dataSource = dataSource;
-	}
+  constructor(dataSource: GTaskDataSource) {
+    this.dataSource = dataSource
+  }
 
-	async get(id: string) {
-		return this.dataSource.get(id);
-	}
+  async get(id: string) {
+    return this.dataSource.get(id)
+  }
 
-	async list() {
-		return this.dataSource.list();
-	}
+  async list() {
+    return this.dataSource.list()
+  }
 
-	async update(id: string, data: Partial<GTaskItem>) {
-		return this.dataSource.update(id, data);
-	}
+  async update(id: string, data: Partial<GTaskItem>) {
+    return this.dataSource.update(id, data)
+  }
 
-	async insert(meta: { title: string; due?: string }) {
-		return this.dataSource.insert(meta);
-	}
+  async insert(meta: { title: string; due?: string }) {
+    return this.dataSource.insert(meta)
+  }
 }
