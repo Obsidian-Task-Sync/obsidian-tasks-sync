b2043f2e8a5e233674a515acbc32f71bfb46ed57
hong-sile <hyukjoon.dev@gmail.com>
2025-06-05 15:10:42 +0900

    feat: TaskRepo 초기화 하는 기능 추가

--- src/repositories/FileRepository.ts
+++ src/repositories/FileRepository.ts
@@ -26,6 +26,26 @@ export class FileRepository {
 
     return file;
   }
+
+  async initialize(): Promise<void> {
+    const markdownFiles = this.app.vault.getMarkdownFiles();
+    const batchSize = 20;
+
+    for (let i = 0; i < markdownFiles.length; i += batchSize) {
+      const batch = markdownFiles.slice(i, i + batchSize);
+      await Promise.all(
+        batch.map(async (tFile) => {
+          const file = new File(this.app, this.remote, tFile);
+          await file.initialize();
+
+          if (file.hasAnyTask()) {
+            this.files.set(tFile.path, file);
+          }
+        }),
+      );
+      await new Promise((resolve) => setTimeout(resolve, 10));
+    }
+  }
 }
 
 interface ScanFileResult {
@@ -44,6 +64,22 @@ export class File {
     private remote: Remote,
     private file: TFile,
   ) {}
+
+  async initialize(): Promise<void> {
+    const content = await this.app.vault.read(this.file);
+    const lines = content.split('\n');
+
+    for (const line of lines) {
+      const meta = getGTaskLineMeta(line);
+
+      if (meta != null) {
+        const { status, title, tasklistId, id } = meta;
+        const task = new Task(id, tasklistId, title, status);
+
+        this.tasks.set(`${id}:${tasklistId}`, task);
+      }
+    }
+  }
 
   getTask(id: string, tasklistId: string): Task | undefined {
     return this.tasks.get(`${id}:${tasklistId}`);
@@ -84,12 +120,17 @@ export class File {
       }
     }
 
+    // iterator 순회중에 중간에 삭제하는 것은 위험하므로, 추후 로직 수정 필요
     for (const task of this.tasks.values()) {
-      if (!lines.some((line) => line.includes(`gtask:${task.tasklistId}:${task.id}`))) {
+      if (!lines.some((line) => line.includes(`gtask:${task.id}:${task.tasklistId}`))) {
         this.tasks.delete(`${task.id}:${task.tasklistId}`);
       }
     }
 
     await Promise.all(result.updated.map((task) => this.remote.update(task.id, task.tasklistId, task)));
+  }
+
+  hasAnyTask(): boolean {
+    return this.tasks.size > 0;
   }
 }
