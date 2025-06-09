06e2d57684bd7e5fb845540ca0e8679993cf6d67
hoondeveloper <hoonskyn9@gmail.com>
2025-06-09 14:30:32 +0900

    빌드 시 test.ts 제외하도록

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
 // vitest.config.ts
-import { defineConfig } from 'vitest/config';
-import { resolve, dirname } from 'path';
+import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
+import { defineConfig } from 'vitest/config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,6 +16,9 @@ export default defineConfig({
       all: true,
       include: ['src/**/*.ts'],
       exclude: ['**/*.test.ts', 'src/views/**'],
+    },
+    typecheck: {
+      tsconfig: './tsconfig.test.json',
     },
   },
   resolve: {
